ce87418f1d2548237bf5ed20225600e17662102c
Jenna Telesca <jenna.telesca@gmail.com>
2016-03-11 12:29:06 -0700

    Made a real-life data table

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
+import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-#decorator follows. route is a function inside the function we created. 
+
+def get_csv():
+	csv_path = './static/la-riots-deaths.csv'
+	csv_file = open(csv_path, 'r')
+	csv_obj = csv.DictReader(csv_file)
+	csv_list = list(csv_obj)
+	return csv_list
+	
+	
+# CSV list coverts to a list	
+# You use DictReader when you have a file of headers. 
+# Each row will be a list and each item will be a dictionary 
+# Where the keys are the headers and the values in that row. 
+# r is read	
+# .= start where you currently are
+# decorator is @app.route('/') route is a function inside the function we created. 
 
 @app.route('/')
-
 def index():
 	template = 'index.html'
-	return render_template(template)
-	
+	object_list = get_csv()
+ 	return render_template(template, object_list=object_list)	
+ 	
 #template is a variable. index.html is a string
 #return is the result of the function 
 
