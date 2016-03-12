48de9068dbb770d96d76f1bbcf4d1504dfec5bab
Jenna Telesca <jenna.telesca@gmail.com>
2016-03-12 10:06:59 -0700

    made a detail page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import csv
 from flask import Flask
+from flask import abort
 from flask import render_template
 app = Flask(__name__)
 
@@ -26,8 +27,21 @@ def index():
 	object_list = get_csv()
  	return render_template(template, object_list=object_list)	
  	
-#template is a variable. index.html is a string
-#return is the result of the function 
+# template is a variable. index.html is a string
+# return is the result of the function 
+
+# making 60 URLs. Below it is what the URL. 
+# there aren't rules for how you define URLs
+
+		    
+@app.route('/<row_id>/')
+def detail(row_id):
+    template = 'detail.html'
+    object_list = get_csv()
+    for row in object_list:
+        if row['id'] == row_id:
+            return render_template(template, object=row)
+    abort(404)
 
 
 if __name__ == '__main__':
